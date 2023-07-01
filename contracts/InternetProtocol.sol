c58f0dbd6a19e14c0bc7daedaca9f5fee067fa15
Nikita Pozdniakov <nikitosing4@mail.ru>
2023-07-01 17:36:32 +0300

    Add rarity fucntions

--- contracts/InternetProtocol.sol
+++ contracts/InternetProtocol.sol
@@ -16,6 +16,23 @@ contract InternetProtocol is
     Ownable
 {
     using Strings for uint256;
+    enum Rarity { Common, Rare, Epic, Legendary }
+    uint32 [] legendaryIds = [
+                                3324575748, // a.root-servers.net	198.41.0.4
+                                3339259593, // b.root-servers.net	199.9.14.201
+                                3223389196, // c.root-servers.net	192.33.4.12
+                                3339148045, // d.root-servers.net	199.7.91.13
+                                3234588170, // e.root-servers.net	192.203.230.10
+                                3221554673, // f.root-servers.net	192.5.5.241
+                                3228574724, // g.root-servers.net	192.112.36.4
+                                3328294453, // h.root-servers.net	198.97.190.53
+                                3223622673, // i.root-servers.net	192.36.148.17
+                                3225059358, // j.root-servers.net	192.58.128.30
+                                3238006401, // k.root-servers.net	193.0.14.129
+                                3339146026, // l.root-servers.net	199.7.83.42
+                                3389791009, // m.root-servers.net	202.12.27.33
+                                2130706433  // 127.0.0.1
+                            ];
 
     uint256 _totalSupply = 4294967296;
     string private head =
@@ -32,6 +49,70 @@ contract InternetProtocol is
     ) public onlyOwner {
         _safeMint(to, tokenId);
         _setTokenURI(tokenId, uri);
+    }
+
+    function determineRarity(uint32 id) internal view returns(Rarity) {
+        uint8 octet1 = uint8(id % (2**8));
+        uint8 octet2 = uint8((id / (2**8)) % (2**8));
+        uint8 octet3 = uint8((id / (2**16)) % (2**8));
+        uint8 octet4 = uint8(id / (2**24));
+        bool isPalindrom = reverseDigits(octet4) == octet1 && reverseDigits(octet3) == octet2;
+        if (checkMembership(id, legendaryIds)) {
+            return Rarity.Legendary;
+        }
+
+        if ((octet1 + 1 == octet2) && (octet2 + 1 == octet3) && (octet3 + 1 == octet4)) {
+            return Rarity.Legendary;
+        } 
+
+        if (areAllSameDigits(octet1) && areAllSameDigits(octet2) && areAllSameDigits(octet3) && areAllSameDigits(octet4)) {
+            uint8 d1 = octet1 % 10;
+            uint8 d2 = octet2 % 10;
+            uint8 d3 = octet3 % 10;
+            uint8 d4 = octet4 % 10;
+            if (d1 == d2 && d2 == d3 && d3 == d4) {
+                return Rarity.Legendary;
+            } else {
+                if (isPalindrom) return Rarity.Epic;
+                return Rarity.Rare;
+            }
+        }
+
+        if ((octet1 % 10 == 0) && (octet2 % 10 == 0) && (octet3 % 10 == 0) && (octet4 % 10 == 0)) {
+            return Rarity.Rare;
+        }
+        
+        if (isPalindrom) {
+            return Rarity.Epic;
+        }
+
+        return Rarity.Common;
+    }
+
+    function reverseDigits(uint8 number) public pure returns (uint8) {
+        uint8 reversed = 0;
+        
+        while (number > 0) {
+            uint8 digit = number % 10;
+            reversed = (reversed * 10) + digit;
+            number /= 10;
+        }
+        
+        return reversed;
+    }
+
+    function areAllSameDigits(uint32 octet) internal pure returns (bool) {
+        uint8 d1 = uint8(octet % 10);
+        uint8 d2 = uint8((octet / 10) % 10);
+        uint8 d3 = uint8((octet / 100) % 10);
+        return d1 == d2 && d2 == d3;
+    }
+
+    function checkMembership(uint32 id, uint32 [] storage ids) internal view returns (bool) {
+        for (uint8 i = 0; i < ids.length; i++) {
+            if (ids[i] == id) return true;
+        }
+        return false;
     }
 
     // The following functions are overrides required by Solidity.
